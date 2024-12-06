b27fd3a033b62e141b493cbc157996ad8d7f3912
Anton suzyy <frixed@icloud.com>
2024-12-06 15:55:40 +0300

    админ панель

--- orders/backend/admin.py
+++ orders/backend/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Product, Cart, Order
+
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(Order)
